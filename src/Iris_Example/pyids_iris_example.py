561e8a7356fb44eb8ac541cc68201ea1e93bdecb
Matthew Cloutier <mrcloutier624@gmail.com>
2025-11-04 14:59:10 -0500

    Best Lambdas example added

--- src/Iris_Example/pyids_iris_example.py
+++ src/Iris_Example/pyids_iris_example.py
@@ -17,6 +17,8 @@ url = "https://raw.githubusercontent.com/kliegr/arcBench/master/data/folds_discr
 s = requests.get(url).content
 df = pd.read_csv(io.StringIO(s.decode('utf-8')))
 cars = mine_CARs(df, rule_cutoff=50)
+print("\n--- Learned Decision Rules ---")
+print(f"Mined CARs Count: {len(cars)}\n")
 lambda_array = [1, 1, 1, 1, 1, 1, 1]
 
 quant_dataframe = QuantitativeDataFrame(df)
@@ -26,14 +28,6 @@ ids.fit(quant_dataframe=quant_dataframe, class_association_rules=cars, lambda_ar
 
 print("\n--- Learned Decision Rules ---")
 print(f"Total Rules Selected by IDS: {len(ids.clf.rules)}\n")
-
-car_idx = 0
-for i in enumerate(ids.clf.rules, 1):
-    car = cars[car_idx]  # retrieve the original CAR from the mined list
-    print(f"Rule {i}:")
-    print(f"  IF {car.antecedent} THEN class = {car.consequent}")
-    print(f"  Support: {car.support}, Confidence: {car.confidence}\n")
-    car_idx+=1
 
 acc = ids.score(quant_dataframe)
 print(f"Accuracy Score: {acc}\n")
