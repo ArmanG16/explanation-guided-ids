0ef061b60275ebfa7e71014adc108b443a99f0ef
Matthew Cloutier <mrcloutier624@gmail.com>
2025-11-07 14:45:57 -0500

    Added print helper

---
+++ src/utils/Print_Helper.py
@@ -0,0 +1,16 @@
+import time
+
+def MyPrint(file_name, message, line_num=-1, error=False, success=False):
+    line_num_msg = ""
+    if (line_num >= 0):
+        line_num_msg = "Line: " + str(line_num) + " "
+
+    color_change = ""
+    if (error):
+        color_change = "\033[91m"
+    elif (success):
+        color_change = "\033[92m"
+
+    timestamp = time.strftime("%H:%M:%S")
+
+    print("\n" + color_change + timestamp + ": " + line_num_msg + message + "\033[0m")
