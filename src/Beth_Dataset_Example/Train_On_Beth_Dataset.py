95ded2a02b3009a52c8748bdb3fa196796054fe6
Matthew Cloutier <mrcloutier624@gmail.com>
2025-11-19 13:38:58 -0500

    Training function, rules CSV, Quantitative df warnings fixed

--- src/Beth_Dataset_Example/Train_On_Beth_Dataset.py
+++ src/Beth_Dataset_Example/Train_On_Beth_Dataset.py
@@ -1,5 +1,4 @@
 import sys
-import glob
 import os
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
@@ -7,57 +6,21 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 sys.path.insert(0, os.path.join(BASE_DIR, "pyIDS"))
 
 data_dir = os.path.join(BASE_DIR, "data/processed/bethdataset")
-cars_dir = os.path.join(BASE_DIR, "data/cars/bethdataset.csv")
+cars_dir = os.path.join(BASE_DIR, "data/cars")
+output_path = os.path.join(BASE_DIR, "data/rules/bethdataset_rules.csv")
+cars_csv_name = "bethdataset.csv"
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 from src.utils.Mining_Cars_Func import Mine_Cars
-from src.utils.CSV_Files_To_DataFrame import CSV_to_DF
-
-from pyids.algorithms.ids import IDS
-import pandas as pd
-
-from pyarc.qcba.data_structures import QuantitativeDataFrame
+from src.utils.Training_Func import Train
 
 def Beth_Train(max_rows):
 
-    cars = Mine_Cars(data_dir, max_rows, cars_dir)
+    cars = Mine_Cars(max_rows, 50, data_dir, cars_dir + "/" + cars_csv_name)
 
     lambda_array = [1, 1, 1, 1, 1, 1, 1]
 
-    df = CSV_to_DF(data_dir, max_rows=max_rows)
-    df['class'] = df['class'].astype(str)
-
-    quant_dataframe = QuantitativeDataFrame(df)
-
-    ids = IDS(algorithm="SLS")
-    ids.fit(quant_dataframe=quant_dataframe, class_association_rules=cars, lambda_array=lambda_array)
-
-    print("\n--- Learned Decision Rules ---")
-    print(f"Total Rules Selected by IDS: {len(ids.clf.rules)}\n")
-
-    print("\n--- Simplified IDS Rule Summaries ---")
-
-    for i, rule in enumerate(ids.clf.rules, start=1):
-        car = rule.car
-
-        antecedent = dict(car.antecedent)
-        consequent = car.consequent
-        confidence = car.confidence
-        support = car.support
-        f1 = getattr(rule, "f1", None)
-
-        print(f"Rule {i}:")
-        print(f"  IF {antecedent}")
-        print(f"  THEN {consequent}")
-        print(f"  Support: {support:.3f}, Confidence: {confidence:.3f}", end="")
-        if f1 is not None:
-            print(f", F1: {f1:.3f}")
-        else:
-            print()
-
-
-    acc = ids.score(quant_dataframe) # accuracy is the percentage of the dataset covered by the generated rules
-    print("\nAccuracy Score: " + str(acc))
+    Train("SLS", lambda_array, cars, max_rows, data_dir, output_path)
 
 if __name__ == "__main__":
     Beth_Train(10000)
---
+++ src/utils/Training_Func.py
@@ -0,0 +1,47 @@
+import sys
+import os
+from src.utils.Print_Helper import MyPrint
+from src.utils.CSV_Files_To_DataFrame import CSV_to_DF
+import pandas as pd
+from pyarc.qcba.data_structures import QuantitativeDataFrame
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+from pyids.algorithms.ids import IDS
+
+def Train(algorithm, lambda_array, cars, max_rows, data_dir, output_path):
+    df = CSV_to_DF(data_dir, max_rows=max_rows)
+    df["class"] = df["class"].astype(str)
+    quant_dataframe = QuantitativeDataFrame(df)
+
+    MyPrint("Training_Func", "Beginning training with pyIDS...")
+    ids = IDS(algorithm=algorithm)
+    ids.fit(quant_dataframe=quant_dataframe, class_association_rules=cars, lambda_array=lambda_array)
+
+    MyPrint("Training_Func", f"Total Rules Selected by IDS: {len(ids.clf.rules)}\n")
+
+    acc = ids.score(quant_dataframe) # accuracy is the percentage of the dataset covered by the generated rules
+
+    rules_list = []
+
+    for i, rule in enumerate(ids.clf.rules, start=1):
+        car = rule.car
+
+        antecedent = dict(car.antecedent)
+        consequent = car.consequent
+        confidence = car.confidence
+        support = car.support
+        f1 = getattr(rule, "f1", None)
+
+        rules_list.append({
+            "Rule_Index": i,
+            "Antecedent": str(antecedent),
+            "Consequent": str(consequent),
+            "Support": support,
+            "Confidence": confidence,
+            "F1": f1,
+            "Accuracy": acc if i == 1 else ""
+        })
+
+    rules_df = pd.DataFrame(rules_list)
+
+    rules_df.to_csv(output_path, index=False)
