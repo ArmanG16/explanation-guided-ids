aa399a854ccb5cffe5c1c712dc635dc24f29a758
Matthew Cloutier <mrcloutier624@gmail.com>
2026-01-29 12:16:28 -0500

    Fixing rebase conflicts

--- src/UNR-IDD/Train_UNR-IDD.py
+++ src/UNR-IDD/Train_UNR-IDD.py
@@ -7,23 +7,21 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 
 sys.path.insert(0, os.path.join(BASE_DIR, "pyIDS"))
 
-data_dir = os.path.join(BASE_DIR, "data/processed/UNR-IDD")
-cars_dir = os.path.join(BASE_DIR, "data/cars")
+data_dir = os.path.join(BASE_DIR, "data/processed/UNR-IDD_preprocessed.csv")
+cars_dir = os.path.join(BASE_DIR, "data/cars/UNR-IDD.csv")
 output_path = os.path.join(BASE_DIR, "data/rules/UNR-IDD_rules.csv")
 lambdas_path = os.path.join(BASE_DIR, "data/lambdas/UNR-IDD_best_lambdas.csv")
-data_dir_name = "UNR-IDD_preprocessed.csv"
-cars_csv_name = "UNR-IDD.csv"
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-from src.utils.Mining_Cars_Func import Mine_Cars
-from src.utils.Training_Func import Train
+from src.pyIDS_Functions.Mining_Cars_Func import Mine_Cars
+from src.pyIDS_Functions.Training_Func import Train
 from src.utils.Optimizing_Lambdas import Optimize_Lambdas
 from src.utils.Print_Helper import MyPrint
 
 def UNR_IDD_Train(max_rows, val_fraction = 0.2, random_state=42):
     MyPrint("Train_UNR-IDD", "Beginning to Train UNR-IDD")
 
-    full_df = pd.read_csv(os.path.join(data_dir, data_dir_name))
+    full_df = pd.read_csv(data_dir)
     full_df = full_df.head(max_rows)
     full_df["class"] = full_df["class"].astype(str)
 
@@ -34,7 +32,7 @@ def UNR_IDD_Train(max_rows, val_fraction = 0.2, random_state=42):
         random_state=random_state
     )
 
-    cars = Mine_Cars(100, train_df, cars_dir + "/" + cars_csv_name)
+    cars = Mine_Cars(100, train_df, cars_dir)
 
     lambda_array = Optimize_Lambdas(
         algorithm="SLS",
